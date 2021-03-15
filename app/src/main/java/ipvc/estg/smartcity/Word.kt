d29af118982f40bbc84e6c1befd1ac1b5e690f03
pereirajoaoEI <61158154+pereirajoaoEI@users.noreply.github.com>
2021-03-15 19:24:03 +0000

    Criação do featuring Notas, com insert na BD

---
+++ app/src/main/java/ipvc/estg/smartcity/Word.kt
@@ -0,0 +1,14 @@
+package ipvc.estg.smartcity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "word_table")
+
+class Word(
+     @PrimaryKey @ColumnInfo(name = "word") val word: String
+)
+
+
+
