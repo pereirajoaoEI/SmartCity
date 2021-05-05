b2600f4a7b49ded5b1270f994b6830217c08982e
pereirajoaoEI <61158154+pereirajoaoEI@users.noreply.github.com>
2021-05-05 21:06:56 +0100

    Anomalias finalizado

--- app/src/main/java/ipvc/estg/smartcity/MenuLogado.kt
+++ app/src/main/java/ipvc/estg/smartcity/MenuLogado.kt
@@ -3,14 +3,34 @@ package ipvc.estg.smartcity
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.content.pm.ActivityInfo
+import android.graphics.Color
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatDelegate
+import com.mikhaellopez.circularprogressbar.CircularProgressBar
 
-class MenuLogado : AppCompatActivity() {
+class MenuLogado : AppCompatActivity(), SensorEventListener {
+
+    private lateinit var  sensorManager: SensorManager
+    private var brightness: Sensor?= null
+    private lateinit var text: TextView
+    private lateinit var pb: CircularProgressBar
+    private lateinit var square: TextView
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     override fun onCreate(savedInstanceState: Bundle?) {
+        
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_logado)
 
@@ -22,7 +42,12 @@ class MenuLogado : AppCompatActivity() {
         )
        bemVindo.setText("Bem-vindo "+sessaoAuto.getString("username", null)+" !")
 
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        text = findViewById(R.id.tv_text)
+        pb = findViewById(R.id.circularProgressBar)
+        square = findViewById(R.id.tv_square)
 
+        setUpSensorStuff()
 
         val button2 = findViewById<ImageButton>(R.id.notas)
         button2.setOnClickListener {
@@ -51,6 +76,80 @@ class MenuLogado : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    private fun setUpSensorStuff(){
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        brightness = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also{
+
+            sensorManager.registerListener(this,
+                    it,
+                    SensorManager.SENSOR_DELAY_FASTEST,
+                    SensorManager.SENSOR_DELAY_FASTEST)
+        }
+    }
+
+    private fun brightness(brightness: Float):String {
+        return when (brightness.toInt()) {
+            0 -> "Totalmente Escuro"
+            in 1..10 -> "Muito Escuro"
+            in 11..50 -> "Escuro"
+            in 51..5000 -> "Normal"
+            in 5001..25000-> "Luminosidade"
+            else -> "Muita Luminosidade"
+
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        return
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if(event?.sensor?.type == Sensor.TYPE_LIGHT){
+            val light = event.values[0]
+
+            text.text = "Sensor: $light\n${brightness(light)}"
+            pb.setProgressWithAnimation(light)
+        }
+
+        if(event?.sensor?.type == Sensor.TYPE_ACCELEROMETER){
+            val sides = event.values[0]
+            val upDown = event.values[1]
+
+            square.apply{
+                rotationX = upDown * 3f
+                rotationY = sides * 3f
+                rotation = -sides
+                translationX = sides * -10
+                translationY = upDown * -10
+            }
+
+            val color = if (upDown.toInt() == 0 && sides.toInt() == 0) Color.GREEN else Color.RED
+            square.setBackgroundColor(color)
+
+            square.text="up/down ${upDown.toInt()}\nleft/right ${sides.toInt()}"
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, brightness, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onDestroy() {
+        sensorManager.unregisterListener(this)
+        super.onDestroy()
     }
 
 }
